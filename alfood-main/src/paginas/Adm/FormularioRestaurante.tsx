4c2bd7c60202628a878053fdf04029ee71e83b19
LeonardoLobas <leonardolobas2703@gmail.com>
2025-05-05 11:55:03 -0300

    feat: melhorar a estrutura do formulário de restaurantes com Material-UI

--- alfood-main/src/paginas/Adm/FormularioRestaurante.tsx
+++ alfood-main/src/paginas/Adm/FormularioRestaurante.tsx
@@ -1,4 +1,4 @@
-import { Button, TextField } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
 import axios from "axios";
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
@@ -58,18 +58,40 @@ const FormularioRestaurante = () => {
     };
 
     return (
-        <form onSubmit={aoSubmeterForm}>
-            <TextField
-                value={nomeRestaurante}
-                onChange={(event) => setNomeRestaurante(event.target.value)}
-                id="standard-basic"
-                label="Restaurante nome"
-                variant="standard"
-            />
-            <Button type="submit" variant="outlined">
-                Salvar
-            </Button>
-        </form>
+        <>
+            <Box
+                sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                }}
+            >
+                <Typography component="h1" variant="h6">
+                    Formulário de Restaurantes
+                </Typography>
+                <Box component="form" onSubmit={aoSubmeterForm}>
+                    <TextField
+                        value={nomeRestaurante}
+                        onChange={(event) =>
+                            setNomeRestaurante(event.target.value)
+                        }
+                        id="standard-basic"
+                        label="Restaurante nome"
+                        variant="standard"
+                        fullWidth
+                        required
+                    />
+                    <Button
+                        sx={{ marginTop: 1 }}
+                        type="submit"
+                        fullWidth
+                        variant="outlined"
+                    >
+                        Salvar
+                    </Button>
+                </Box>
+            </Box>
+        </>
     );
 };
 
